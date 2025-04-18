b36a9bd2a51bc62f933fb0b1d688e49c7969deba
Capstone_GreenGuard <larasayodya04@gmail.com>
2025-04-18 15:29:02 +0700

    perbaikan dependensi

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:loginfe/home.dart';
+import 'home.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
